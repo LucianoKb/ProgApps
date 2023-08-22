4ed7b55c357610d46ef3abe5f5bc75525cb49d10
Luciano <e.gonzalez0024@gmail.com>
2023-08-22 20:23:03 -0300

    se quitan repetidos

--- prueba/src/main/java/logica/Socio.java
+++
@@ -1,9 +0,0 @@
-package logica;
-
-import datatypes.DtFecha;
-
-class Socio extends Usuario {
-    public Socio(String nickname, String nombre, String apellido, String email, DtFecha fechaNac) {
-        super(nickname, nombre, apellido, email, fechaNac);
-    }
-}
